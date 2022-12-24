525187a8d1f145ed1b56e596812685ac88014965
AsakaLynux <asakalynux@gmail.com>
2022-12-24 21:49:41 +0700

    make actor table

--- app/Http/Controllers/ActorController.php
+++ app/Http/Controllers/ActorController.php
@@ -6,16 +6,5 @@ use Illuminate\Http\Request;
 
 class ActorController extends Controller
 {
-    public function createActor(Request $request) {
-
-        $validateData = $request->validate([
-            'name' => 'required|min:3',
-            'gender' => 'required',
-            'biography' => 'required|min:10',
-            'dob' => 'required',
-            'place_of_birth' => 'required',
-            'image_url' => 'required|image|mimes:jpg,png,jpeg,gif',
-            'popularity' => 'required|numeric',
-        ]);
-    }
+    //
 }
