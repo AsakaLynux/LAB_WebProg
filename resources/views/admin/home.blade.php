2dbfad27f97809e9ce90310c4b6e04d95e79af29
AsakaLynux <asakalynux@gmail.com>
2023-01-09 20:22:50 +0700

    fix caraousel add admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,83 +4,91 @@
 <!-- Carousel -->
 <div id="carouselExampleCaptions" class="carousel slide">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        @foreach ($movies as $key => $movie)
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{$key}}" class="{{$key == 0 ? 'active' : '' }}"
+        aria-current="{{$key == 0 ? 'true' : '' }}"></button>
+        @endforeach
     </div>
     <div class="carousel-inner">
-        @foreach ($movies as $movie)
-        <div class="carousel-item active">
+        @foreach ($movies as $key => $movie)
+        <div class="carousel-item {{$key == 0 ? 'active' : '' }}">
             <img src="{{$movie->background}}" class="d-block w-100 carousel-img" alt="...">
             <div class="carousel-caption d-none d-md-block">
-              <h1>{{$movie->title}}</h1>
-              <h5>{{$movie->release_date}} | {{$movie->genre}}</h5>
-              <p>{{$movie->description}}</p>
+                <h1>{{$movie->title}}</h1>
+                <h5>{{$movie->release_date}} | {{$movie->genre}}</h5>
+                <p>{{$movie->description}}</p>
             </div>
-          </div>
+        </div>
         @endforeach
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-</div>
-
-<!-- Popular -->
-<div class="d-flex py-2 px-3 bg-dark">
-    <img class="me-2 pt-2 popular-img" src="storage/images/assets/flame.png" alt="">
-    <h2 class="text-white">Popular</h2>
-</div>
-<div class="d-flex justify-content-center">
-    @foreach ($movies as $movie)
-    <div class="mx-3">
-        <img class="cover-img" src="{{$movie->image_thumbnail}}" alt="">
-        <a class="cover-text" href="/detail-movies/{{$movie->id}}"><h5 class="text-white cover-text">{{$movie->title}}</h5></a>
-        <p class="text-secondary cover-text">{{$movie->release_date}}</p>
-    </div>
-    @endforeach
-
-</div>
-
-<!-- Show -->
-<div class="d-flex py-2 px-3 bg-dark">
-    <div class="p-2 d-flex">
-        <img class="me-2 pt-2 show-img" src="storage/images/assets/film.png" alt="">
-        <h2 class="text-white">Show</h2>
-    </div>
-    <div class="d-flex">
-        <form action="/admin" method="get">
-
-            <input type="text" name="searchMovie" class="form-control bg-secondary text-white"
-                id="exampleInputEmail1" aria-describedby="emailHelp">
-            <button type="submit">Search</button>
-        </form>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </div>
-<div class="d-flex justify-content-center mt-3">
-    @foreach ($genres as $genre)
+    <!-- Popular -->
+    <div class="d-flex py-2 px-3 bg-dark">
+        <img class="me-2 pt-2 popular-img" src="storage/images/assets/flame.png" alt="">
+        <h2 class="text-white">Popular</h2>
+    </div>
+    <div class="d-flex justify-content-center">
+        @foreach ($movies as $movie)
+        <div class="mx-3">
+            <img class="cover-img" src="{{$movie->image_thumbnail}}" alt="">
+            <a class="cover-text" href="/detail-movies/{{$movie->id}}">
+                <h5 class="text-white cover-text">{{$movie->title}}</h5>
+            </a>
+            <p class="text-secondary cover-text">{{$movie->release_date}}</p>
+        </div>
+        @endforeach
+
+    </div>
+
+    <!-- Show -->
+    <div class="d-flex py-2 px-3 bg-dark">
+        <div class="p-2 d-flex">
+            <img class="me-2 pt-2 show-img" src="storage/images/assets/film.png" alt="">
+            <h2 class="text-white">Show</h2>
+        </div>
+        <div class="d-flex">
+            <form action="/admin" method="get">
+
+                <input type="text" name="searchMovie" class="form-control bg-secondary text-white"
+                    id="exampleInputEmail1" aria-describedby="emailHelp">
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center mt-3">
+        @foreach ($genres as $genre)
         <a href="#"><button class="bg-dark show-btn">{{$genre->genre}}</button></a>
         @endforeach
-</div>
-<div class="d-flex mt-3">
-    <p class="mx-3 pt-2 text-white">Sort by</p>
-    <a href=""><button class="bg-dark show-btn">Latest</button></a>
-    <a href=""><button class="bg-dark show-btn">A - Z</button></a>
-    <a href=""><button class="bg-dark show-btn">Z - A</button></a>
-</div>
-<div class="d-flex mx-3 mt-3 text-white">
-    <a href="/create-movie"><button class="btn btn-danger"><img width="20" height="20" src="storage/images/assets/plus-btn.png" alt=""> Add Movie</button></a>
-</div>
-<div class="d-flex justify-content-center">
-    @foreach ($movies as $movie)
-    <div class="mx-3">
-        <img class="cover-img" src="{{$movie->image_thumbnail}}" alt="">
-        <a class="cover-text" href="/detail-movies/{{$movie->id}}"><h5 class="text-white cover-text">{{$movie->title}}</h5></a>
-        <p class="text-secondary cover-text">{{$movie->release_date}}</p>
     </div>
-    @endforeach
-</div>
-  @endsection
+    <div class="d-flex mt-3">
+        <p class="mx-3 pt-2 text-white">Sort by</p>
+        <a href=""><button class="bg-dark show-btn">Latest</button></a>
+        <a href=""><button class="bg-dark show-btn">A - Z</button></a>
+        <a href=""><button class="bg-dark show-btn">Z - A</button></a>
+    </div>
+    <div class="d-flex mx-3 mt-3 text-white">
+        <a href="/create-movie"><button class="btn btn-danger"><img width="20" height="20"
+                    src="storage/images/assets/plus-btn.png" alt=""> Add Movie</button></a>
+    </div>
+    <div class="d-flex justify-content-center">
+        @foreach ($movies as $movie)
+        <div class="mx-3">
+            <img class="cover-img" src="{{$movie->image_thumbnail}}" alt="">
+            <a class="cover-text" href="/detail-movies/{{$movie->id}}">
+                <h5 class="text-white cover-text">{{$movie->title}}</h5>
+            </a>
+            <p class="text-secondary cover-text">{{$movie->release_date}}</p>
+        </div>
+        @endforeach
+    </div>
+    @endsection
--- resources/views/layout/layout-admin.blade.php
+++ resources/views/layout/layout-admin.blade.php
@@ -8,6 +8,7 @@
     <script src="js/bootstrap.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>MovieList</title>
 </head>
@@ -37,6 +38,15 @@
 
     @yield('contents')
 
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul class="mb-0">
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     <footer class="bg-dark text-center text-white mt-5">
         <!-- Logo buat footer -->
         <div>
@@ -58,7 +68,31 @@
             <a class="footer-text" href="#">About Us</a>
         </div>
         <!-- Copyright footer -->
-        <div class="text-center p-3 footer-text" style="background-color: rgba(0, 0, 0, 0.2);">Copyright © 2021<img class="footer-movielogo" src="assets/movielist-logo.png" alt=""></div>
+        <div class="text-center p-3 footer-text" style="background-color: rgba(0, 0, 0, 0.2);">Copyright © 2021<img class="footer-movielogo" src="storage/images/assets/movielist-logo.png" alt=""></div>
     </footer>
+
 </body>
+<script>
+    $('.extra-fields').click(function() {
+    $('.records').clone().appendTo('.records_dynamic');
+    $('.records_dynamic .records').addClass('single remove');
+    $('.single .extra-fields').remove();
+    $('.single').append('<a href="#" class="remove-field btn-remove-customer">Remove Fields</a>');
+    $('.records_dynamic > .single').attr("class", "remove");
+
+    $('.records_dynamic input').each(function() {
+        var count = 0;
+        var fieldname = $(this).attr("name");
+        $(this).attr('name', fieldname + count);
+        count++;
+    });
+
+    });
+
+    $(document).on('click', '.remove-field', function(e) {
+        $(this).parent('.remove').remove();
+        e.preventDefault();
+    });
+
+</script>
 </html>
