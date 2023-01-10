ca684bc739f3ca653ec289a81a2647a13ff63e79
AsakaLynux <asakalynux@gmail.com>
2023-01-10 16:38:45 +0700

    Make better image

--- resources/views/layout/layout-user.blade.php
+++ resources/views/layout/layout-user.blade.php
@@ -31,7 +31,7 @@
                     <a class="navbar-text" href="/watchlist/{{$user->id}}">Watchlist</a>
                     <div class="dropdown dropdown-profile rounded-circle">
                         <a class="" href="#" data-bs-toggle="dropdown" data-bs-theme="dark" aria-expanded="false"><img
-                                src="storage/images/assets/profile-pict.jpg" width="40" height="40"
+                                src="{{$user->image_url != null ? $user->image_url : storage/images/assets/profile-pict.jpg}}" width="40" height="40"
                                 class="rounded-circle"></a>
                         <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
                             <li><a class="dropdown-item" href="/profile/{{$user->id}}">Profile</a></li>
